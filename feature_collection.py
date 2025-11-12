680724c20b480ece2377b6a41b31a873d15004d4
ALZUAA <m.alzuaa@student.maastrichtuniversity.nl>
2025-11-12 18:36:56 +0100

    added cpu data object/collector, changed the class design for data collector and added new abstract data object.

--- feature_collection.py
+++ feature_collection.py
@@ -1,26 +1,28 @@
 from abc import ABC, abstractmethod
 import sys
 
+class Data_Object(ABC):
+    pass
 # watch that all the formats that come out of 
 # the functions (for the respective OSs) are the same
-class Model_Feature(ABC):
+class Model_Feature_Collector(ABC):
     # ONLY windows, mac and linux are supported
     SUPPORTED_OPERATING_SYSTEMS_FOR_DATA_COLLECTION = ['win32', 'darwin', 'linux'] #refer to sys.platform docs 
     @abstractmethod
-    def collectFromWindows(self, csv_file_name):
+    def collectFromWindows(self) -> Data_Object:
         pass
     @abstractmethod
-    def collectFromMac(self, csv_file_name):
+    def collectFromMac(self) -> Data_Object:
         pass
     @abstractmethod
-    def collectFromLinux(self, csv_file_name):
+    def collectFromLinux(self) -> Data_Object:
         pass
-    def collectFeatureAndPutIn(self, csv_file_name):
+    def collectFeature(self) -> Data_Object:
         if sys.platform == self.SUPPORTED_OPERATING_SYSTEMS_FOR_DATA_COLLECTION[0]:
-            self.collectFromWindows(csv_file_name)
+            return self.collectFromWindows()
         elif sys.platform == self.SUPPORTED_OPERATING_SYSTEMS_FOR_DATA_COLLECTION[1]:
-            self.collectFromMac(csv_file_name)
+            return self.collectFromMac()
         elif sys.platform == self.SUPPORTED_OPERATING_SYSTEMS_FOR_DATA_COLLECTION[2]:
-            self.collectFromLinux(csv_file_name)
+            return self.collectFromLinux()
         else:
             raise NotImplementedError("Data collection for feature is not supported for the following platform: " + sys.platform)
